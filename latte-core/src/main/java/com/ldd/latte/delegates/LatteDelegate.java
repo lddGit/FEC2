121eb66a0650445e5685f255019704bef4e6ae22
ldd <1452229540@qq.com>
2020-03-31 11:40:25 +0800

    2020.3.31 采用单Activity框架，多Fragment的形式。 model:latte-core

---
+++ latte-core/src/main/java/com/ldd/latte/delegates/LatteDelegate.java
@@ -0,0 +1,11 @@
+package com.ldd.latte.delegates;
+
+/**
+ * Created by 14522 on 2020/3/31.
+ */
+//项目正式使用的delegate
+public abstract class LatteDelegate extends PermissionCheckerDelegate {
+
+
+
+}
---
+++ latte-core/src/main/java/com/ldd/latte/delegates/PermissionCheckerDelegate.java
@@ -0,0 +1,9 @@
+package com.ldd.latte.delegates;
+
+/**
+ * Created by 14522 on 2020/3/31.
+ */
+
+public abstract class PermissionCheckerDelegate extends BaseDelegate {
+
+}
